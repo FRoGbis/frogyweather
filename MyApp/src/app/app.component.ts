167fbaa574961b2a292ae93b36c5dc26f51860f8
FRoGbis <bebel.franck@gmail.com>
2016-11-30 13:44:56 +0100

    changement dans le menu

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -21,8 +21,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Accueil', component: Page1 },
+      { title: 'Upload', component: Page2 },
+      //{ title: 'Graphiques', component: Page3},
     ];
 
   }
