3fdf91e5f147496ee6d94353ccdd993afa7dde3d
FRoGbis <bebel.franck@gmail.com>
2016-12-15 15:32:17 +0100

    ajout liste fichier page upload et delete

--- myAppTabs/www/js/controllers.js
+++ myAppTabs/www/js/controllers.js
@@ -4,6 +4,10 @@ angular.module('starter.controllers', [])
   $scope.files = File.get();
 })
 
-.controller('UploadsCtrl', function($scope) {})
+.controller('UploadsCtrl', function($scope, File) {
+  $scope.files = File.get();
+})
 
-.controller('DeleteCtrl', function($scope) {})
+.controller('DeleteCtrl', function($scope, File) {
+  $scope.files = File.get();
+})
