bab961403eeab8aa5f771ca9ce58c2e64f3a3eaf
FRoGbis <bebel.franck@gmail.com>
2016-12-15 13:38:59 +0100

    ajout de la liste des fichiers sur la premiere page

--- myAppTabs/www/js/controllers.js
+++ myAppTabs/www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('FilesCtrl', function($scope) {})
+.controller('FilesCtrl', function($scope, File) {
+  $scope.files = File.get();
+})
 
 .controller('UploadsCtrl', function($scope) {})
 
